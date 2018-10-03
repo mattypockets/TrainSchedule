74f22e7b40a4143b61f451d89a046cfd0b891298
Matt Gasper <mrgasper27@gmail.com>
2018-10-03 12:32:32 -0500

    Created basic HTML layout. Began JS.

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCq4aaFSqNdgiBvgtkHvWgmCMx-JE9HmKw",
+        authDomain: "train-scheduler-bf041.firebaseapp.com",
+        databaseURL: "https://train-scheduler-bf041.firebaseio.com",
+        projectId: "train-scheduler-bf041",
+        storageBucket: "",
+        messagingSenderId: "766007837568"
+      };
+      firebase.initializeApp(config);
+    
+    var dataRef = firebase.database();
+
+    // Take inputs from form and save them to Firebase
+
+    
+
+    // Calculate time until next train
+
+    // Calculate when next train will arrive
+
+    // Display info on DOM
+});
