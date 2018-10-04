7c33fb1e5a5732ac4e66690df6c5198ebca7505a
Matt Gasper <mrgasper27@gmail.com>
2018-10-03 19:32:21 -0500

    Added - On click, save to Firebase

--- javascript/script.js
+++ javascript/script.js
@@ -14,12 +14,39 @@ $(document).ready(function(){
     var dataRef = firebase.database();
 
     // Take inputs from form and save them to Firebase
+    $(".submit-button").on("click", function(event){
+        event.preventDefault();
 
-    
+        // Set variables for each input field
+        var trainName = $(".trainName").val().trim();
+        var destination = $(".destination").val().trim();
+        var firstTime = moment($(".firstTime").val().trim(), "HH:mm").format("X");
+        var frequency = $(".frequency").val().trim();
+
+        // Bundle variables into an object
+        var newTrain = {
+            trainName: trainName,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency
+        }
+
+        // Push object to Firebase
+        dataRef.ref().push(newTrain);
+
+        // Clear form fields
+        $(".trainName").val("")
+        $(".destination").val("")
+        $(".firstTime").val("")
+        $(".frequency").val("")
+
+    })
+
 
     // Calculate time until next train
 
     // Calculate when next train will arrive
 
     // Display info on DOM
+    
 });
